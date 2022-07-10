8e17be2d8dbd30356aa559738cbf508d5c0df1e7
Victor Rodrigues <91481122+DevzVictor@users.noreply.github.com>
2022-07-10 09:28:43 -0300

    add auth middleware

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -10,7 +10,7 @@ const loginController = async (req, res) => {
   if (!user) {
     return res.status(400).send({ message: "Usuário não encontrado!" });
   }
-
+ 
   const isPasswordValid = await bcrypt.compare(password, user.password);
 
   if (!isPasswordValid) {
