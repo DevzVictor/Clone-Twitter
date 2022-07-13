cb3fe5959444ded7e4bf94e15547c083cd7c88a4
Victor Rodrigues <91481122+DevzVictor@users.noreply.github.com>
2022-07-12 22:13:45 -0300

    add search tweets

--- src/tweets/tweets.services.js
+++ src/tweets/tweets.services.js
@@ -4,9 +4,18 @@ const createTweetService = (message, userId) => {
   return Tweet.create({ message, user: userId });
 };
 
-const findAllTweetsService = () => Tweet.find().sort({ _id: -1 }).populate("user");
+const findAllTweetsService = () =>
+  Tweet.find().sort({ _id: -1 }).populate("user");
+
+const searchTweetService = (message) =>
+  Tweet.find({
+    message: { $regex: `${message || ""}`, $options: "i" },
+  })
+    .sort({ _id: -1 })
+    .populate("user");
 
 module.exports = {
   createTweetService,
   findAllTweetsService,
+  searchTweetService,
 };
